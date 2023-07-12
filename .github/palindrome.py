7ede691f8557d1ea0122368cfe8d21225355bf14
Advika Prashanth <advika.prashanth08@gmail.com>
2023-07-12 09:06:49 -0400

    Create palindrome.py

---
+++ .github/palindrome.py
@@ -0,0 +1,25 @@
+class Solution:
+    # Write code below to complete prompt
+    def isPalindrome(self, s):
+            #type s: string
+            #return type: boolean
+            
+            #TODO: Write code below to return a boolean value with the solution to the prompt.
+            if s.len <= 6:
+                 return False 
+            count = 0 
+            length = s.len/2
+            for i in range (length) :
+                if s[count] != s[-count]:
+                     return False 
+                count = count +1
+            pass
+
+def main():
+    tc1 = Solution()
+    inpyt = input()
+    # Write code below to complete prompt
+    print(tc1.isPalindrome(inpyt))
+
+if __name__ == "__main__":
+    main()
