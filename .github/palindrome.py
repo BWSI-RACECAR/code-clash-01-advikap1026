d390c5bc9167c6642494f0dac8426ab4ee509355
Advika Prashanth <advika.prashanth08@gmail.com>
2023-07-12 09:09:33 -0400

    Update palindrome.py

--- .github/palindrome.py
+++ .github/palindrome.py
@@ -5,14 +5,20 @@ class Solution:
             #return type: boolean
             
             #TODO: Write code below to return a boolean value with the solution to the prompt.
+            bool = True  
             if s.len <= 6:
-                 return False 
+                 bool = False 
             count = 0 
             length = s.len/2
             for i in range (length) :
                 if s[count] != s[-count]:
-                     return False 
+                     bool = False 
                 count = count +1
+
+            if bool == False:
+                 return False
+            else:
+                 return True
             pass
 
 def main():
